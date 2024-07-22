466329c54ed5acb4080677c0cbb222eb3929a846
Massimo Di Pierro <massimo.dipierro@gmail.com>
2024-07-21 22:06:35 -0700

    bazelrc

--- projects/project3/test.cpp
+++ projects/project3/test.cpp
@@ -5,6 +5,13 @@ using Eigen::MatrixXd;
  
 int main()
 {
+    std::string cxx =
+#ifdef __clang__
+   "clang++";
+#else
+   "g++";
+#endif
+  std::cout << "The C++ compiler version is: " << cxx << std::endl;
   MatrixXd m(2,2);
   m(0,0) = 3;
   m(1,0) = 2.5;
